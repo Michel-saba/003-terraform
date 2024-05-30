acafef1b4b87f431990f8350a5cebc383e054adc
Chadi Saba <48120950+Michel-saba@users.noreply.github.com>
2024-05-30 19:41:19 +0000

    create s3 public bucket

--- main.tf
+++ main.tf
@@ -14,15 +14,31 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-3"
 }
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "bucket-name"
+  bucket = "bucket-example"
 
-  region = "us-west-2"
+
   tags = {
-    public_bucket = false
+    public_bucket = true
   }
+}
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
 }
